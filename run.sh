626394f88e9ce0bd852c48407e33a41d8111dacf
Vicente Insfran <vinsfran@gmail.com>
2018-03-07 13:00:14 -0300

    Se agrega run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run -d -t \
+        --name swagger-example \
+        -v /opt/apps/swagger-example:/src \
+        -w /src \
+        -p 9002:8080 \
+        --dns 192.168.129.111 \
+        --dns-search asuncion.gov.py \
+        devapps.asuncion.gov.py:5000/asu/maven:3.3.9 \
+        /src/mvn spring-boot:run
